8fc47238bbe60fd27438efc7a5f4772d482909c4
erikrios <erikriosetiawan49@gmail.com>
2020-03-15 20:52:50 +0700

    Add service builder

--- app/src/main/java/com/erikriosetiawan/myproductapp/data/api/ServiceBuilder.kt
+++ app/src/main/java/com/erikriosetiawan/myproductapp/data/api/ServiceBuilder.kt
@@ -1,8 +1,33 @@
 package com.erikriosetiawan.myproductapp.data.api
 
 import com.erikriosetiawan.myproductapp.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ServiceBuilder {
 
+    // Base url
+    private const val URL = BuildConfig.BASE_URL
 
+    // Create logger
+    private val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    // Create OkHttpClient
+    private val okHttp = OkHttpClient.Builder()
+        .callTimeout(15, TimeUnit.SECONDS)
+        .addInterceptor(logger)
+
+    // Create Retrofit Builder
+    private val builder = Retrofit.Builder()
+        .baseUrl(URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttp.build())
+
+    // Create Retrofit Instance
+    private val retrofit = builder.build()
+
+    fun <T> buildService(serviceType: Class<T>): T = retrofit.create(serviceType)
 }
