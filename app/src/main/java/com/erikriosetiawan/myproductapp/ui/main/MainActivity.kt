12a683f225d37a21538b7328177fae6447cb8375
erikrios <erikriosetiawan49@gmail.com>
2020-03-15 20:07:10 +0700

    Renew the list item

--- app/src/main/java/com/erikriosetiawan/myproductapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/myproductapp/ui/main/MainActivity.kt
@@ -3,7 +3,10 @@ package com.erikriosetiawan.myproductapp.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.erikriosetiawan.myproductapp.R
+import com.erikriosetiawan.myproductapp.adapter.ProductAdapter
+import com.erikriosetiawan.myproductapp.data.model.Product
 import com.erikriosetiawan.myproductapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +15,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,
+        binding = DataBindingUtil.setContentView(
+            this,
             R.layout.activity_main
         )
+        testRecyclerView()
+    }
+
+    private fun testRecyclerView() {
+        val products: MutableList<Product> = mutableListOf()
+        for (i in 0..20) {
+            products.add(
+                Product(i, "Produk $i", "Rp. ${i}.000,00")
+            )
+        }
+        binding.recyclerViewProduct.apply {
+            adapter = ProductAdapter(this@MainActivity, products)
+            layoutManager =
+                LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+        }
     }
 }
