e1870def02de382d2ed788656acb7eed6fb1045f
erikrios <erikriosetiawan49@gmail.com>
2020-03-15 22:54:56 +0700

    Implements view model and live data in main activity

--- app/src/main/java/com/erikriosetiawan/myproductapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/myproductapp/ui/main/MainActivity.kt
@@ -3,12 +3,18 @@ package com.erikriosetiawan.myproductapp.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.erikriosetiawan.myproductapp.R
+import com.erikriosetiawan.myproductapp.adapter.ProductAdapter
 import com.erikriosetiawan.myproductapp.databinding.ActivityMainBinding
+import com.erikriosetiawan.myproductapp.ui.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +22,28 @@ class MainActivity : AppCompatActivity() {
             this,
             R.layout.activity_main
         )
+        setUpViewModel()
+    }
+
+    private fun setUpViewModel() {
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
+
+    private fun setUpUI() {
+        // Observe the LiveData
+        viewModel.apply {
+            isShow.observe(this@MainActivity, Observer {
+                binding.swipeMain.isRefreshing = it
+            })
+            products.observe(this@MainActivity, Observer {
+                val adapter = ProductAdapter(this@MainActivity, it)
+                binding.recyclerViewProduct.layoutManager =
+                    LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+                binding.recyclerViewProduct.adapter = adapter
+                adapter.notifyDataSetChanged()
+            })
+        }
+
+        binding.swipeMain.setOnRefreshListener { viewModel.getDataProducts() }
     }
 }
