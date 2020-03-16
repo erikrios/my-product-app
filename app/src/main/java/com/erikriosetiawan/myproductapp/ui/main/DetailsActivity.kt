3f8f02377b6c85ddd47c90a95d054fbb1874ca05
erikrios <erikriosetiawan49@gmail.com>
2020-03-16 08:38:37 +0700

    Integrated the data binding

--- app/src/main/java/com/erikriosetiawan/myproductapp/ui/main/DetailsActivity.kt
+++ app/src/main/java/com/erikriosetiawan/myproductapp/ui/main/DetailsActivity.kt
@@ -2,12 +2,16 @@ package com.erikriosetiawan.myproductapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.erikriosetiawan.myproductapp.R
+import com.erikriosetiawan.myproductapp.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_details)
     }
 }
