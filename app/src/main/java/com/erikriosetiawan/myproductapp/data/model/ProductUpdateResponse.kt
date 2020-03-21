7566de6a0d409f8bb24de72c53b69ddf34ba778d
erikrios <erikriosetiawan49@gmail.com>
2020-03-21 19:06:11 +0700

    Add product update response

---
+++ app/src/main/java/com/erikriosetiawan/myproductapp/data/model/ProductUpdateResponse.kt
@@ -0,0 +1,31 @@
+package com.erikriosetiawan.myproductapp.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductUpdateResponse(
+    @SerializedName("status")
+    val status: Int,
+    @SerializedName("error")
+    val error: String? = null,
+    @SerializedName("response")
+    val response: Response
+)
+
+data class Response(
+    @SerializedName("fieldCount")
+    val fieldCount: Int,
+    @SerializedName("affectedRows")
+    val affectedRows: Int,
+    @SerializedName("insertId")
+    val insertId: Int,
+    @SerializedName("serverStatus")
+    val serverStatus: Int,
+    @SerializedName("warningCount")
+    val warningCount: Int,
+    @SerializedName("message")
+    val message: String,
+    @SerializedName("protocol41")
+    val protocol41: Boolean,
+    @SerializedName("changedRows")
+    val changedRows: Int
+)
